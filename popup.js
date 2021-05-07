a31b4bd07f68e79063f1994800fd73f30bf21fbc
Diógenes Dauster <diogenes.dauster@gmail.com>
2021-05-07 01:05:28 -0300

    alterações diversas

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ let btnGerarPessoa = document.getElementById("gerarPessoa");
 let btnFillForm1 = document.getElementById("fillForm1");
 let btnFillForm2 = document.getElementById("fillForm2");
 
+
 btnFillForm2.addEventListener("click", async () => {
   const currentPage = await getCurrentTab();
 
@@ -10,10 +11,17 @@ btnFillForm2.addEventListener("click", async () => {
     target: { tabId: currentPage.id },
     function: preencheForm2,
   });
+
+  chrome.storage.sync.get("persons", (data) => {
+    let count = String(data.persons.length);
+    chrome.action.setBadgeText({ text: count });
+  });
+
 });
 
 btnFillForm1.addEventListener("click", async () => {
   const currentPage = await getCurrentTab();
+
 
   await chrome.scripting.executeScript({
     target: { tabId: currentPage.id },
@@ -59,7 +67,7 @@ async function preencheForm1() {
 
     nome.value = person.nome;
     email.value = person.email;
-    $(telefone).inputmask("setvalue", person.celular);
+    telefone.value = person.celular;
   });
 }
 
@@ -73,7 +81,7 @@ async function preencheForm2() {
   const cep = document.querySelector("[os-test-input=cep]");
   const numero = document.querySelector("[os-test-input=numero]");
 
-  chrome.storage.sync.get("persons", async (data) => {
+  await chrome.storage.sync.get("persons", async (data) => {
     const last = data.persons.length - 1;
     const person = data.persons[last];
 
@@ -81,17 +89,17 @@ async function preencheForm2() {
     email.value = person.email;
     cemail.value = person.email;
 
-    $(telefone).inputmask("setvalue", person.celular);
-    $(cpf).inputmask("setvalue", person.cpf);
-    $(dtnascimento).inputmask("setvalue", person.data_nasc);
-    $(cep).inputmask("setvalue", person.cep);
+    telefone.value = person.celular;
+    cpf.value = person.cpf;
+    dtnascimento.value =  person.data_nasc.replaceAll("/","-");
+    cep.value = person.cep;
     numero.value = person.numero;
 
     data.persons.pop();
     chrome.storage.sync.set({ persons: [...data.persons] });
-    const count = String(last);
-    chrome.action.setBadgeText({ text: count });
+    
   });
+
 }
 
 async function gerarPessoa() {
